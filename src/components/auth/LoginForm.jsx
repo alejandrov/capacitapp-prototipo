5c0ed5c1d8b04fbc55851b46f6f8d6e7a7deabdc
Alejandro V. <villarroel@gmail.com>
2025-04-01 10:41:37 -0600

    update version number in LoginForm to v10.41

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -75,7 +75,7 @@ const LoginForm = ({ onSubmit, loading = false, error = null }) => {
           </Link>
          
         </div>
-        <small>v10.29</small>
+        <small>v10.41</small>
       </form>
     </div>
   );
